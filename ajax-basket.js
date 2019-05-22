3e863e6fd857966aafc31ffb7544755d3fd07831
emanuele <emanuele.biotti@gmail.com>
2019-05-22 14:25:02 +0200

    Impostazione esercizio

---
+++ ajax-basket.js
@@ -0,0 +1,23 @@
+// Utilizzare l’API: https://www.boolean.careers/api/array/basket?n=numberPlayers
+// Chiedere all’utente quanti giocatori vuole generare.
+// Chiedere all’API i giocatori e stampare a schermo una card per ogni giocatore attraverso handlebars.
+// Inizialmente chiamate l'API con un numero fisso stabilito da voi, in modo da interrogare l'API nel modo corretto e stilare le card.
+// Poi aggiungete la richiesta del numero dei giocatori all'utente
+
+$.ajax({
+
+  'url': 'https://www.boolean.careers/api/array/basket',
+  'method': 'GET',
+  'data': {
+    'n': 3
+  },
+  'success': function(data){
+    var giocatore = data.response;
+    console.log(giocatore);
+
+  },
+  'error': function(){
+    alert('errore');
+  }
+
+});
