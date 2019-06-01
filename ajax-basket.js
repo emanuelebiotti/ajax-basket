e589cb7ea697c3408c37a8eca20c02448d76bfe1
emanuele <emanuele.biotti@gmail.com>
2019-06-01 12:04:30 +0200

    Esercizio ultimato

--- ajax-basket.js
+++ ajax-basket.js
@@ -1,54 +1,47 @@
-// Utilizzare l’API: https://www.boolean.careers/api/array/basket?n=numberPlayers
-// Chiedere all’utente quanti giocatori vuole generare.
-// Chiedere all’API i giocatori e stampare a schermo una card per ogni giocatore attraverso handlebars.
-// Inizialmente chiamate l'API con un numero fisso stabilito da voi, in modo da interrogare l'API nel modo corretto e stilare le card.
-// Poi aggiungete la richiesta del numero dei giocatori all'utente
 $(document).ready(function(){
 
-  var source   = $("#entry-template").html();
-  var template = Handlebars.compile(source);
+var num_giocatori;
+
+var source   = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+$('input').keypress(function(event){
+  if(event.which == 13) {
+    $('.card_container').empty();
+    ricerca();
+    $('input').val('');
+  }
+})
+
+function ricerca() {
+  num_giocatori = $('input').val();
 
   $.ajax({
-
-    'url': 'https://www.boolean.careers/api/array/basket',
+    'url':'https://www.boolean.careers/api/array/basket?',
     'method': 'GET',
     'data': {
-      'n': 24
+      'n': num_giocatori
     },
-    'success': function(data){
-      console.log(data.response);
+    'success':function(info){
+      console.log(info.response);
 
-      $('input').keypress(function(event){
-      // se il tasto premuto è "invio"
-        if(event.which == 13) {
-        var numerogiocatori = $('input').val();
-
-          for (var i = 0; i < numerogiocatori; i++) {
-
-            var context = {
-              playerCode: data.response[i].playerCode,
-              rebounds: data.response[i].rebounds,
-              fouls: data.response[i].fouls,
-              points: data.response[i].points,
-              twoPoints: data.response[i].twoPoints,
-            };
-
-
-            var html = template(context);
-            $('.container').append(html);
-            $('input').val('');
-
-          }
+      for (var i = 0; i <num_giocatori; i++) {
+        var giocatore = {
+          'fouls': info.response[i].fouls,
+          'playerCode': info.response[i].playerCode,
+          'points': info.response[i].points,
+          'rebounds': info.response[i].rebounds,
+          'threePoints': info.response[i].threePoints,
+          'twoPoints': info.response[i].twoPoints
         }
-    });
-
+        var html = template(giocatore);
+        $('.card_container').append(html);
+      }
     },
     'error': function(){
       alert('errore');
     }
-
-  });
-
-
+  })
+}
 
 });
